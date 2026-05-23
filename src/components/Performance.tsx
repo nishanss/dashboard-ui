23d1b513256aa1821e522fe760d40e485edeb841
Nishansanad <nishansanad@gmail.com>
2026-05-23 12:00:14 +0400

    initialized individual teacher page

---
+++ src/components/Performance.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
+import { Sector, Cell, ResponsiveContainer } from "recharts";
+import Image from "next/image";
+
+// #region Sample data
+const data = [
+  { name: "Group A", value: 92, fill: "#C3EBFA" },
+  { name: "Group B", value: 8, fill: "#FAE27C" },
+];
+
+const Performance = () => {
+  return (
+    <div className="bg-white p-4 rounded-md h-80">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Performance</h1>
+        <Image src="/moreDark.png" alt="" width={16} height={16} />
+      </div>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={data}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Performance;
